0610cb915de2d3fbbe99e0c8d772ef1473c99789
Cat Yanish <catyanish@Cats-MacBook-Air.local>
2017-09-17 23:55:28 -0500

    added code comments

--- server/public/scripts/controllers/message.controller.js
+++ server/public/scripts/controllers/message.controller.js
@@ -2,7 +2,7 @@ myApp.controller('MessageController', ['$http', function($http) {
   var mc = this;
 
 
-//this object stores new info to be posted on messages
+//this object stores new info to be posted on messages in an existing template
   mc.message = {
     firstName: "",
     lastName: "",
@@ -10,7 +10,7 @@ myApp.controller('MessageController', ['$http', function($http) {
     type: ""
   };
 
-
+//this object takes data to create a new template
 mc.custom = {
   firstName: "Candy",
   lastName: "Pace",
@@ -18,7 +18,7 @@ mc.custom = {
   body: "Thank you for staying with us! We are here to help, so let any one of our staff know how we can make your trip great"
 }
 
-
+//makes a request to create a message from existing template with guest info
   mc.sendMessage = function() {
     $http.post('/guest', mc.message).then(function(response) {
       console.log('created message', response);
@@ -26,7 +26,7 @@ mc.custom = {
   }// end post
 
 customMessage();
-
+//makes a request to create a new template with guest info
   function customMessage() {
     $http.post('/guest/custom', mc.custom).then(function(response) {
       console.log('created message', response);
